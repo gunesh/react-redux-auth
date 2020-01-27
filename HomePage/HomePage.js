f53413835fb7959e6f87012c925bc4b75f610ed6
gunesh <gunesh26@gmail.com>
2020-01-27 18:02:56 +0530

    1 file modified

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -1,38 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { userActions } from '../_actions';
+import { userActions } from "../_actions";
 
 class HomePage extends React.Component {
-    componentDidMount() {
-        this.props.getUsers();
-    }
+  componentDidMount() {
+    this.props.getUsers();
+  }
 
-    handleDeleteUser(id) {
-        return (e) => this.props.deleteUser(id);
-    }
+  handleDeleteUser(id) {
+    return e => this.props.deleteUser(id);
+  }
 
-    render() {
-        const { user, users } = this.props;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-               This is Home Page
+  render() {
+    const { user, users } = this.props;
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              Start Bootstrap
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+<li className="nav-item active">
+            <a className="nav-link" href="#">Home
+              <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
+          </li>
+              </ul>
             </div>
-        );
-    }
+          </div>
+        </nav>{" "}
+      </>
+    );
+  }
 }
 
 function mapState(state) {
-    const { users, authentication } = state;
-    const { user } = authentication;
-    return { user, users };
+  const { users, authentication } = state;
+  const { user } = authentication;
+  return { user, users };
 }
 
 const actionCreators = {
-    getUsers: userActions.getAll,
-    deleteUser: userActions.delete
-}
+  getUsers: userActions.getAll,
+  deleteUser: userActions.delete
+};
 
-const connectedHomePage = connect(mapState, actionCreators)(HomePage);
+const connectedHomePage = connect(
+  mapState,
+  actionCreators
+)(HomePage);
 export { connectedHomePage as HomePage };
