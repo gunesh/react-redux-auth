2b47952b01e6cc63fe6617494f09cac79a839191
gunesh <gunesh26@gmail.com>
2020-01-28 19:15:39 +0530

    1 file modified

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -1,111 +1,59 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import { userActions } from "../_actions";
+import { userActions } from '../_actions';
 
 class HomePage extends React.Component {
-  componentDidMount() {
-    this.props.getUsers();
-  }
+    componentDidMount() {
+        this.props.getUsers();
+    }
 
-  handleDeleteUser(id) {
-    return e => this.props.deleteUser(id);
-  }
+    handleDeleteUser(id) {
+        return (e) => this.props.deleteUser(id);
+    }
 
-  render() {
-    const { user, users } = this.props;
-    return (
-      <>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-          <div className="container">
-            <a className="navbar-brand" href="#">
-              Start Bootstrap
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarResponsive"
-              aria-controls="navbarResponsive"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">
-                    Home
-                    <span className="sr-only">(current)</span>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="register">
-                    Register
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="login">
-                    Login
-                  </a>
-                </li>
-              </ul>
+    render() {
+        const { user, users } = this.props;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h1>Hi {user.firstName}!</h1>
+                <p>You're logged in with React!!</p>
+                <h3>All registered users:</h3>
+                {users.loading && <em>Loading users...</em>}
+                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+                {users.items &&
+                    <ul>
+                        {users.items.map((user, index) =>
+                            <li key={user.id}>
+                                {user.firstName + ' ' + user.lastName}
+                                {
+                                    user.deleting ? <em> - Deleting...</em>
+                                    : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
+                                    : <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span>
+                                }
+                            </li>
+                        )}
+                    </ul>
+                }
+                <p>
+                    <Link to="/login">Logout</Link>
+                </p>
             </div>
-          </div>
-        </nav>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-3">
-              <h1 className="my-4">Shop Name</h1>
-              <div className="list-group">
-                <a href="#" className="list-group-item">
-                  Category 1
-                </a>
-                <a href="#" className="list-group-item">
-                  Category 2
-                </a>
-                <a href="#" className="list-group-item">
-                  Category 3
-                </a>
-              </div>
-            </div>
-
-            <div className="col-lg-9">ddfdff</div>
-          </div>
-        </div>
-        <footer className="py-5 bg-dark">
-          <div className="container">
-            <p className="m-0 text-center text-white">
-              Copyright &copy; Your Website 2019
-            </p>
-          </div>
-        </footer>
-      </>
-    );
-  }
+        );
+    }
 }
 
 function mapState(state) {
-  const { users, authentication } = state;
-  const { user } = authentication;
-  return { user, users };
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return { user, users };
 }
 
 const actionCreators = {
-  getUsers: userActions.getAll,
-  deleteUser: userActions.delete
-};
+    getUsers: userActions.getAll,
+    deleteUser: userActions.delete
+}
 
-const connectedHomePage = connect(
-  mapState,
-  actionCreators
-)(HomePage);
+const connectedHomePage = connect(mapState, actionCreators)(HomePage);
 export { connectedHomePage as HomePage };
